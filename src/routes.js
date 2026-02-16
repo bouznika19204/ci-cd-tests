7e7aced291c57e3115634109b8b870a8b4e2139a
Ahmidou om <ahmidouctx@gmail.com>
2026-02-16 10:29:38 +0100

    MAJ

--- src/routes.js
+++ src/routes.js
@@ -12,5 +12,11 @@ router.get('/time', (req, res) => {
     res.json({ time: now.toISOString() })
 })
 
+router.get('/welcome', (req, res) => {
+const name = req.query.name || 'Visiteur'
+// VULNÉRABILITÉ : On renvoie l'input utilisateur sans filtrage
+res.send(`<h1>Bienvenue, ${name}</h1>`)
+})
+
 module.exports = router
 
